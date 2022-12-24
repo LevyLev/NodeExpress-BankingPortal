14511fd4656c137535cc984b5d629143f2e1f46a
Levent Cadir <cadirlevent98@gmail.com>
2022-12-24 12:14:55 +0200

    module2

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const { render } = require('ejs');
 
 const app = express();
 app.use(express.static(path.join(__dirname, './public')));
@@ -8,9 +9,47 @@ app.use(express.static(path.join(__dirname, './public')));
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
+const accountData = fs.readFileSync(
+  path.join(__dirname, './json/accounts.json'),
+  {
+    encoding: 'UTF8',
+  }
+);
+const userData = fs.readFileSync(path.join(__dirname, './json/users.json'), {
+  encoding: 'UTF8',
+});
+
+const accounts = JSON.parse(accountData);
+const users = JSON.parse(userData);
+
 app.get('/', (req, res) => {
   res.render('index', {
-    title: 'Index',
+    title: 'Account Summary',
+    accounts,
+  });
+});
+
+app.get('/savings', (req, res) => {
+  res.render('account', {
+    account: accounts.savings,
+  });
+});
+
+app.get('/checking', (req, res) => {
+  res.render('account', {
+    account: accounts.checking,
+  });
+});
+
+app.get('/credit', (req, res) => {
+  res.render('account', {
+    account: accounts.credit,
+  });
+});
+
+app.get('/profile', (req, res) => {
+  res.render('profile', {
+    user: users[0],
   });
 });
 
