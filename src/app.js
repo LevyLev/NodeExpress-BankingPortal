dea6026dd63fd17bb59019f2c503832dca18f88d
Levent Cadir <cadirlevent98@gmail.com>
2022-12-24 14:27:44 +0200

    module4

--- src/app.js
+++ src/app.js
@@ -3,12 +3,18 @@ const path = require('path');
 const express = require('express');
 const { accounts, users, writeJSON } = require('./data');
 
+const accountsRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
+
 const app = express();
 app.use(express.static(path.join(__dirname, './public')));
 app.use(express.urlencoded({ extended: true }));
 
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
+
+app.use('/accounts', accountsRoutes);
+app.use('/services', servicesRoutes);
 
 app.get('/', (req, res) => {
   res.render('index', {
@@ -17,58 +23,9 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/savings', (req, res) => {
-  res.render('account', {
-    account: accounts.savings,
-  });
-});
-
-app.get('/checking', (req, res) => {
-  res.render('account', {
-    account: accounts.checking,
-  });
-});
-
-app.get('/credit', (req, res) => {
-  res.render('account', {
-    account: accounts.credit,
-  });
-});
-
 app.get('/profile', (req, res) => {
   res.render('profile', {
     user: users[0],
-  });
-});
-
-app.get('/transfer', (req, res) => {
-  res.render('transfer');
-});
-
-app.post('/transfer', (req, res) => {
-  accounts[req.body.from].balance -= req.body.amount;
-  accounts[req.body.to].balance += parseInt(req.body.amount);
-
-  writeJSON();
-
-  res.render('transfer', {
-    message: 'Transfer Completed',
-  });
-});
-
-app.get('/payment', (req, res) => {
-  res.render('payment', { account: accounts.credit });
-});
-
-app.post('/payment', (req, res) => {
-  accounts.credit.balance -= req.body.amount;
-  accounts.credit.available += parseInt(req.body.amount);
-
-  writeJSON();
-
-  res.render('payment', {
-    message: 'Payment Successful',
-    account: accounts.credit,
   });
 });
 
